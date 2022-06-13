1fbb4271f81f1fc745f33c7c8a662fcd5d3b99f9
jonatan <jonatanmeza01@gmail.com>
2022-06-12 21:05:04 -0300

    update files. Homework JSI complete. Homework JS II in progress.

---
+++ 02-JS-I/homework/homework.md
@@ -0,0 +1,27 @@
+1. En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos como si se lo estuvieras explicando a un niño de 12 años. Hacer esto te ayudará a descubrir rápidamente cualquier agujero en tu comprensión.
+
+- Variables
+- Strings
+- Funciones (argumentos, `return`)
+- Declaraciones `if`
+- Valores booleanos (`true`, `false`)
+
+Variable: Nos permite guardar algo para luego recuperar o utilizar el mismo. a la variable le asignamos valores. En Js podmeos crear variables de 3 formas diferentes;
+var, let y const.
+ejemplo1: var nombre = "jonatan"
+ejemplo2: const nombre = "jonatan"
+ejemplo3: let nombre = "jonatan"
+
+Las variables declaradas como var, puden ser modificadas el valor. Lo mismo pasa con las variables let. A diferencia de las anteriores, las variabels const, no se pueden modificar el valor (salvo arrays y objetos).
+
+String: es un tipo de dato primitivo que utiliza cadena de texto. Siguiendo el ejemplo anterior, la variable nombre posee un valor de tipo string, que es jonatan. Este tipo de datos lo veremos siempre entre comillas, ya sean simples o dobles.
+
+Funciones (argumentos, `return`): Las funciones son elementos que contienen un conjunto de instrucciones dentro. La misma puede ser llamada colocando el nombre de la función más parentesis, ejemplo: mifuncion(), y esta función ejecutará las instrucciones que contienen dentro.
+
+Las funciones pueden contener argumentos, que permitirá luego darle un valor, cuando llamamos a la función.
+
+La palabra clave return nos permitirá retornar un valor, y terminar la ejecución de la función.
+
+Declaraciones `if`: Nos permiten crear código con condiciones, por ejemplo si a + b = 10, entonces pasa algo, sino, pasa otra cosa.
+
+Valores booleanos (`true`, `false`): Son los que se utilizan en las condiciones antes mencionadas, en el ejemplo anterior si a vale 5 y b vale 5, entonces al sumar nos daría 10, y esto sería un valor true para el if. Si esa condición no diera como resultado 10, en el if el valor sería false.
--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,20 +5,39 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x > y) {
+    return x;
+  } else if (x === y) {
+    return x;
+  } else {
+    return y;
+  }
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad >= 18) {
+    return "Allowed";
+  } else {
+    return "Not allowed";
+  }
 }
-  
+
 function conection(status) {
-  //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
+  //Recibimos un estado de conexión de un usuario representado por un valor numérico.
   //Cuando el estado es igual a 1, el usuario está "Online"
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if (status === 1) {
+    return "Online";
+  } else if (status === 2) {
+    return "Away";
+  } else {
+    return "Offline";
+  }
 }
 
 function saludo(idioma) {
@@ -28,6 +47,15 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma === "aleman") {
+    return "Guten Tag!";
+  } else if (idioma === "mandarin") {
+    return "Ni Hao!";
+  } else if (idioma === "ingles") {
+    return "Hello!";
+  } else {
+    return "Hola!";
+  }
 }
 
 function colors(color) {
@@ -38,18 +66,39 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  if (color === "blue") {
+    return "This is blue";
+  } else if (color === "red") {
+    return "This is red";
+  } else if (color === "green") {
+    return "This is green";
+  } else if (color === "orange") {
+    return "This is orange";
+  } else {
+    return "Color not found";
+  }
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero === 10 || numero === 5) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero > 20 && numero < 50) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function esEntero(numero) {
@@ -60,6 +109,11 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if (numero === Math.floor(numero)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function fizzBuzz(numero) {
@@ -67,15 +121,56 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (numero % 3 === 0 && numero % 5 === 0) {
+    console.log("fizzbuzz");
+    return "fizzbuzz";
+  }
+  if (numero % 3 === 0) {
+    console.log("fizz");
+    return "fizz";
+  } else if (numero % 5 === 0) {
+    console.log("buzz");
+    return "buzz";
+  } else {
+    console.log(numero);
+    return numero;
+  }
 }
 
 function operadoresLogicos(num1, num2, num3) {
-  //La función recibe tres números distintos. 
+  //La función recibe tres números distintos.
   //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
   //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
-  //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
+  //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
+  if (num1 === 0 || num2 === 0 || num3 === 0) {
+    console.log("Error");
+    return "Error";
+  }
+  //La función recibe tres números distintos.
+  //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
+  if (num1 < 0 || num2 < 0 || num3 < 0) {
+    console.log("Hay negativos");
+    return "Hay negativos";
+  }
+  //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
+  else if (num1 > num2 && num1 > num3 && num1 > 0) {
+    console.log("Número 1 es mayor y positivo");
+    return "Número 1 es mayor y positivo";
+  }
+  //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
+  else if (num3 > num1 && num3 > num2) {
+    num3++;
+    console.log(num3);
+    return num3;
+  }
+  //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
+  else {
+    console.log("FALSE");
+    return false;
+  }
 }
 
 function esPrimo(numero) {
@@ -86,24 +181,34 @@ function esPrimo(numero) {
   // Nota: Los números 0 y 1 NO son considerados números primos
 }
 
-function esVerdadero(valor){
-  //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
+function esVerdadero(valor) {
+  //Escribe una función que reciba un valor booleano y retorne “Soy verdadero”
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+  if (valor === true) {
+    console.log("Soy verdadero");
+    return "Soy verdadero";
+  } else {
+    console.log("Soy falso");
+    return "Soy falso";
+  }
 }
 
-function tablaDelSeis(){
+function tablaDelSeis() {
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí   
-  
+  //Escribe tu código aquí
+  const table = [];
+  for (i = 0; i <= 10; i++) {
+    table.push(i * 6);
+  }
+  console.log(table);
+  return table;
 }
 
-function tieneTresDigitos(numero){
+function tieneTresDigitos(numero) {
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
 }
 
 function doWhile(numero) {
@@ -111,7 +216,6 @@ function doWhile(numero) {
   //Retornar el valor final.
   //Usar el bucle do ... while.
 }
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
@@ -131,5 +235,5 @@ module.exports = {
   esVerdadero,
   tablaDelSeis,
   tieneTresDigitos,
-  doWhile
+  doWhile,
 };
